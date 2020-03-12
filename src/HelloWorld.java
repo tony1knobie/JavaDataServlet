a25fda7e0b2c0c6c23f5b25d10e6750701b2165b
Braden Ripple <bradenripple@gmail.com>
2020-03-12 16:08:51 -0700

    only now is needed to either add jsp to convert to json

---
+++ src/HelloWorld.java
@@ -0,0 +1,36 @@
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class HelloWorld extends HttpServlet {
+	
+	public ReadXML rd = new ReadXML();
+   private String message;
+
+   public void init() {
+      // Do required initialization
+      try {
+		message = rd.getContents();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}// "Hello Bitches!";
+   }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
+   }
+
+   public void destroy() {
+      // do nothing.
+   }
+}
